865277d18d1858db03a25a33f35a30041bd584ed
Nina Zyabrina <zyabrina95@gmail.com>
2024-09-12 09:59:19 +0200

    checkout settings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './styles.scss';
 import 'bootstrap';
-// import * as bootstrap from 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
+const bodyClasses = ['d-flex', 'flex-cloumn', 'min-vh-100'];
+document.body.classList.add(...bodyClasses);
+
+const p = document.createElement('p');
+p.innerText = 'Начните читать RSS сегодня! Это легко, это красиво.';
+document.body.appendChild(p);
